859c8f10482c0c6ad73027a76477b5021041475d
Yan Xu <yan.xu@sas.com>
2023-04-11 20:56:11 +0000

    Completed module

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -7,4 +7,15 @@ def load_module(directory, name):
     importlib.import_module(name)
     sys.path.pop(0)
 
+
+def load_directory(directory):
+    for path in Path(directory).rglob("*.py"):
+        module = path.stem
+        load_module(str(path.parent), module)
+
+
+def load_bundled():
+    directory = Path(__file__).parent / "bundled"   
+    load_directory(directory)
+
     
