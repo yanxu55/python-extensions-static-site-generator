2b1336bf8a520041e829a6e14671a346d76cbd86
Yan Xu <yan.xu@sas.com>
2023-04-11 21:19:02 +0000

    Completed module

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -8,3 +8,15 @@ def register(hook, order=0):
     return register_callback
 
 
+def event(hook, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
+
+
+def filter(hook, value, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            value = func(value, *args)
+
+    return value
