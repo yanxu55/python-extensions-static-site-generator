cd595734d6f079896a724126aaab41e3bcef8e9d
Yan Xu <yan.xu@sas.com>
2023-04-12 20:12:18 +0000

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -59,7 +59,8 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(path, dest, html["html_body"])
+        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
