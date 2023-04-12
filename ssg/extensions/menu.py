87fa13a805accf0a0f5872d5eedf518d12e444bb
Yan Xu <yan.xu@sas.com>
2023-04-12 20:04:47 +0000

    Completed module

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -11,3 +11,12 @@ def collect_files(source, site_parsers):
                 files.append(path)
                 
 
+
+@hooks.register("generate_menu")
+def generate_menu(html, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
+    menu_item = lambda name, ext: template.format(name, ext, name.title())
+    menu = "\n".join([menu_item(path.stem, ext) for path in files])
+    return "<ul>\n{}</ul>\n{}".format(menu, html)
+
+
