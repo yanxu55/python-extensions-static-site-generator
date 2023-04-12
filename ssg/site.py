8572dd4871ba775e13cb3834c3452f529ebec282
Yan Xu <yan.xu@sas.com>
2023-04-12 20:06:50 +0000

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from ssg import extensions
+from ssg import extensions, hooks
 
 
 class Site:
@@ -29,6 +29,7 @@ class Site:
 
     def build(self):
         extensions.load_bundled()
+        hooks.event("collect_files", self.source, self.parsers)
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
