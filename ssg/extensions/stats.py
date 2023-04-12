da3a7f47135fd5c1f449f79b15b22b790e810d91
Yan Xu <yan.xu@sas.com>
2023-04-12 20:27:30 +0000

    Completed module

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -12,9 +12,19 @@ def start_build():
     start_time = time.time()
 
 
-@hooks.register("stats")
+@hooks.register("written")
 def written():
     global total_written
     total_written += 1
 
+
+@hooks.register("stats")
+def stats():
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
+    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file".format(
+        total_written, final_time, average
+    )
+    print(report)
     
+
