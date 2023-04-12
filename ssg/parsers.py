70c9d730c3c5ae5fc68dd855eb3a18dcdf70ec20
Yan Xu <yan.xu@sas.com>
2023-04-12 20:10:42 +0000

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -46,7 +46,7 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        filtered = hooks.filter("generate_html", html, self.base_ext)
+        filtered = hooks.filter("generate_menu", html, self.base_ext)
         self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
